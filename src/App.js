c3b2fe66fe36955df4cc062b01e0d97663744836
Lucie Caillat <lucie.caillat@gmail.com>
2023-05-29 19:33:09 +0200

    add Userpage

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./Pages/HomePage";
 import SignIn from "./Pages/SignIn";
+import UserPage from "./Pages/UserPage";
 
 export default function App() {
   return (
     <React.Fragment>
       <Header />
-      <HomePage />
+      <UserPage />
       <Footer />
     </React.Fragment>
   );
