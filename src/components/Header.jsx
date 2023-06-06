e75dfaf6746319463a1e425b626ce4291a06e4ff
Lucie Caillat <lucie.caillat@gmail.com>
2023-06-06 12:08:24 +0200

    add Link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,21 @@
 import argentBankLogo from "../img/argentBankLogo.png";
+import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <nav className="main-nav">
-      <a className="main-nav-logo" href="./index.html">
+      <Link className="main-nav-logo" to="/">
         <img
           className="main-nav-logo-image"
           src={argentBankLogo}
           alt="Argent Bank Logo"
         />
         <h1 className="sr-only">Argent Bank</h1>
-      </a>
+      </Link>
       <div>
-        <a className="main-nav-item" href="./sign-in.html">
+        <Link className="main-nav-item" to="./sign-in">
           <i className="fa fa-user-circle"></i>
           Sign In
-        </a>
+        </Link>
       </div>
     </nav>
   );
